845d85e41503734e0fec8ff53242fdcfa09b1ad2
sureeratk <sureerat@skru.ac.th>
2018-08-27 15:47:19 +0700

    veradic function and reflect

--- sum/sum.go
+++ sum/sum.go
@@ -1,6 +1,6 @@
 package main
 
-func Sum(numbers [5]int) int {
+func Sum(numbers []int) int {
 	sum := 0
 	/* for i := 0; i < 5; i++ {
 		sum += numbers[i]
@@ -9,4 +9,13 @@ func Sum(numbers [5]int) int {
 		sum += number
 	}
 	return sum
+}
+
+func SumAll(numbersToSum ...[]int) []int {
+	lengthOfNumbers := len(numbersToSum)
+	sums := make([]int, lengthOfNumbers)
+	for i, numbers := range numbersToSum {
+		sums[i] = Sum(numbers)
+	}
+	return sums
 }
