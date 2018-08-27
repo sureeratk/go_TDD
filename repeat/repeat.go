416d5dea99ca5e77e1f372982cc04af2f71f7390
sureeratk <sureerat@skru.ac.th>
2018-08-27 15:06:21 +0700

    change to using STL lib strings

--- repeat/repeat.go
+++ repeat/repeat.go
@@ -1,9 +1,13 @@
 package iteration
 
+import "strings"
+
 func Repeat(character string, itr int) string {
-	var repeated string
-	for i := 0; i < itr; i++ {
-		repeated += character
-	}
-	return repeated
+	//var repeated string
+	// for i := 0; i < itr; i++ {
+	// 	repeated += character
+	//return repeated
+
+	return strings.Repeat(character, itr)
+
 }
