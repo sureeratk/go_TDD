6f2556d46fa49159441ad301a53b8e0a1bdc1987
sureeratk <sureerat@skru.ac.th>
2018-08-29 12:17:35 +0700

    add methods

--- perimeter/perimeter_test.go
+++ perimeter/perimeter_test.go
@@ -2,7 +2,7 @@ package main
 
 import "testing"
 
-func TestPerimeter(t *testing.T) {
+/* func TestPerimeter(t *testing.T) {
 	rectangle := Rectangle{10.0, 10.0}
 	got := Perimeter(rectangle)
 	want := 40.0
@@ -10,13 +10,28 @@ func TestPerimeter(t *testing.T) {
 	if got != want {
 		t.Errorf("got %.2f want %.2f", got, want)
 	}
-}
+} */
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{12.0, 6.0}
-	got := Area(rectangle)
-	want := 72.0
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
-	}
+
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{12, 6}
+		got := rectangle.Area()
+		want := 72.0
+
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10}
+		got := circle.Area()
+		want := 314.16
+
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	})
+
 }
