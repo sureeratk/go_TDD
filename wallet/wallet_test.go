01b19d667bc3a50d98872de669108f546f12e830
sureeratk <sureerat@skru.ac.th>
2018-08-31 13:54:38 +0700

    Use pointer

--- wallet/wallet_test.go
+++ wallet/wallet_test.go
@@ -1,12 +1,17 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestWallet(t *testing.T) {
 
 	wallet := Wallet{}
 
 	wallet.Deposit(10)
+
+	fmt.Printf("address of balance in test is %v \n", &wallet.balance)
 
 	got := wallet.Balance()
 	want := 10
