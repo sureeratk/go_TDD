8bdcfa73a33b0c75a1cb3b9acd12a306fb4bcb93
sureeratk <sureerat@skru.ac.th>
2018-09-03 11:22:43 +0700

    refactor - using errcheck and add assertNoError

--- wallet/wallet_test.go
+++ wallet/wallet_test.go
@@ -15,10 +15,11 @@ func TestWallet(t *testing.T) {
 
 	t.Run("Withdraw", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
-		wallet.Withdraw(Bitcoin(10))
+		err := wallet.Withdraw(Bitcoin(10))
 		//fmt.Printf("address of balance in test is %v \n", &wallet.balance)
 
 		assertBalance(t, wallet, Bitcoin(10))
+		assertNoError(t, err)
 	})
 
 	t.Run("Withdraw insufficient funds", func(t *testing.T) {
@@ -37,6 +38,13 @@ func assertBalance(t *testing.T, wallet Wallet, want Bitcoin) {
 
 	if got != want {
 		t.Errorf("got %s want %s", got, want)
+	}
+}
+
+func assertNoError(t *testing.T, got error) {
+	t.Helper()
+	if got != nil {
+		t.Fatal("got an error but didnt want one")
 	}
 }
 
